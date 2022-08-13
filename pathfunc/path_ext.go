52df2181d4bf645d2afafb08665a6508496002ce
coder206 <code206@foxmail.com>
2022-08-13 21:00:33 +0800

    增加cutpathext

--- pathfunc/path_ext.go
+++ pathfunc/path_ext.go
@@ -12,3 +12,11 @@ func TrimPathExt(path string) string {
 	}
 	return strings.TrimSuffix(path, ext)
 }
+
+func CutPathExt(path string) (string, string) {
+	ext := filepath.Ext(path)
+	if ext == "" {
+		return path, ""
+	}
+	return strings.TrimSuffix(path, ext), ext
+}
